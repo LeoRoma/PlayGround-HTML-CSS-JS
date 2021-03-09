9db042104f1066903fef27443be144448cd867b0
LeoRoma <xiajtn@gmail.com>
2021-03-09 11:15:33 +0000

    Experiment failed, going for canvas

--- ChangingPositions/src/script.js
+++ ChangingPositions/src/script.js
@@ -30,30 +30,58 @@ let timeout = 1000;
 function changeCircle(){
     for(let i = 0; i < numberContainers.length; i++){
         const numberContainer = numberContainers[i];
-        setTimeout(function(){moveRight(numberContainer)}, timeout);
+        setTimeout(function(){
+            if(i < circleBreakPoint){
+                moveTopRight(numberContainer);
+            }else if(i >= circleBreakPoint && i < (circleBreakPoint * 2)){
+                moveBottomRight(numberContainer);
+            }else if(i >= circleBreakPoint*2 && i < (circleBreakPoint * 3)){
+                moveBottomLeft(numberContainer);
+            }
+        }, timeout);
         timeout += 1000;
     }
  
-    changeSquare()
-    console.log(numberContainers.length -1)
+    // changeSquare()
+    // console.log(numberContainers.length -1)
 }
-
-function moveRight(numberContainer){
+let x = 500;
+let y = 0
+function moveTopRight(numberContainer){
     console.log(numberContainer)
-    numberContainer.classList.add('move-left');
+    // numberContainer.classList.add('move-left');
     numberContainer.style.borderRadius = "50%";
+    numberContainer.style.transform = `translate(${x}px,${y}px)`;
+    console.log(x += 100);
+}
+let x1 = 700;
+let y1 = 0
+
+function moveBottomRight(numberContainer){
+    numberContainer.style.borderRadius = "50%";
+    numberContainer.style.transform = `translate(${x1}px,${y1}px)`;
+    console.log(x1 -= 100);
 }
 
-function changeSquare(){
-    console.log("change square")
-    for(let i = numberContainers.length -1; i >= 0; i--){
-        const numberContainer = numberContainers[i];
-        setTimeout(function(){moveLeft(numberContainer)}, timeout);
-        timeout += 1000;
-    }
-}
+let x2 = 700;
+let y2 = 0
 
-function moveLeft(numberContainer){
-    numberContainer.classList.remove('move-left');
-    numberContainer.style.borderRadius = "0%";
+function moveBottomLeft(numberContainer){
+    numberContainer.style.borderRadius = "50%";
+    numberContainer.style.transform = `translate(${x2}px,${y2}px)`;
+    console.log(x2 -= 100, y -= 100);
 }
+// function changeSquare(){
+//     console.log("change square")
+//     for(let i = numberContainers.length -1; i >= 0; i--){
+//         const numberContainer = numberContainers[i];
+//         setTimeout(function(){moveLeft(numberContainer)}, timeout);
+//         timeout += 3000;
+//     }
+// }
+
+// function moveLeft(numberContainer){
+//     numberContainer.classList.remove('move-left');
+//     numberContainer.style.borderRadius = "0%";
+//     numberContainer.style.border = "green solid"
+// }
