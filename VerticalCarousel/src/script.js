a7509c367e413c31d5e5ea8f91f3abf33d3acc10
LeoRoma <xiajtn@gmail.com>
2021-03-09 20:04:51 +0000

    added div track

--- VerticalCarousel/src/script.js
+++ VerticalCarousel/src/script.js
@@ -2,6 +2,20 @@ const buttonUp = document.querySelector('.button-up');
 const buttonDown = document.querySelector('.button-down');
 const carouselContainer = document.querySelector('.carousel-container');
 const cardContainerLength = document.querySelectorAll('.card-container').length;
+const cardContainer = document.querySelector('.card-container');
+const track = document.querySelector('.track');
 console.log(cardContainerLength)
 
+const carouselContainerHeight = carouselContainer.clientHeight;
+console.log(carouselContainerHeight)
+
 let cardIndex = 0;
+
+buttonUp.addEventListener('click', () => {
+    console.log('hello');
+    if(cardIndex < cardContainerLength){
+        console.log("hi")
+        cardIndex++;
+        track.style.transform = `translateY(-${cardIndex} * ${carouselContainerHeight}px)`;
+    }
+})
