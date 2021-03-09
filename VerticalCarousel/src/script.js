8f0e955c4acfea0c00456b3b36b0e31ff096cd12
LeoRoma <xiajtn@gmail.com>
2021-03-09 18:08:14 +0000

    set base for the carousel

---
+++ VerticalCarousel/src/script.js
@@ -0,0 +1,7 @@
+const buttonUp = document.querySelector('.button-up');
+const buttonDown = document.querySelector('.button-down');
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselContainerLength = document.querySelectorAll('div').length;
+console.log(carouselContainerLength)
+
+let cardIndex = 0;
