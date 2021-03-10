b9bd622b988712bccd24fb1fcb01b1f6633571c2
LeoRoma <xiajtn@gmail.com>
2021-03-10 09:54:02 +0000

    Added second carousel

--- VerticalCarousel/src/script.js
+++ VerticalCarousel/src/script.js
@@ -1,35 +1,33 @@
 const buttonUp = document.querySelector('.button-up');
 const buttonDown = document.querySelector('.button-down');
-const carouselContainer = document.querySelector('.carousel-container');
-const cardContainerLength = document.querySelectorAll('.card-container').length;
-const cardContainer = document.querySelector('.card-container');
-const track = document.querySelector('.track');
-console.log(cardContainerLength)
 
+const carouselContainer = document.querySelector('.carousel-container');
 const carouselContainerHeight = carouselContainer.clientHeight;
-console.log(carouselContainerHeight)
+
+const cardContainerRightLength = document.querySelectorAll('.card-container-right').length;
+
+
+const sliderLeft = document.querySelector('.slider-left');
+const sliderRight = document.querySelector('.slider-right');
 
 let cardIndex = 0;
 
+sliderLeft.style.top = `-${(cardContainerRightLength - 1) * carouselContainerHeight}px`;
 
 buttonUp.addEventListener('click', () => {
-
-    if (cardIndex === cardContainerLength - 1) {
+    if (cardIndex === cardContainerRightLength - 1) {
         cardIndex = -1;
-        console.log("zero", cardIndex)
     }
     cardIndex++;
-
-    track.style.transform = `translateY(-${cardIndex * carouselContainerHeight}px)`;
+    sliderRight.style.transform = `translateY(-${cardIndex * carouselContainerHeight}px)`;
+    sliderLeft.style.transform = `translateY(${cardIndex * carouselContainerHeight}px)`;
 })
 
 buttonDown.addEventListener('click', () => {
-    console.log(cardIndex, " down");
     if (cardIndex === 0) {
-        cardIndex = cardContainerLength
+        cardIndex = cardContainerRightLength
     }
     cardIndex--;
-    track.style.transform = `translateY(-${cardIndex * carouselContainerHeight}px)`;
-
-
+    sliderRight.style.transform = `translateY(-${cardIndex * carouselContainerHeight}px)`;
+    sliderLeft.style.transform = `translateY(${cardIndex * carouselContainerHeight}px)`;
 })
