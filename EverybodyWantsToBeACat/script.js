1e123e1fda75537b05cbac6e5ee5ed754f2fbda0
LeoRoma <xiajtn@gmail.com>
2021-03-15 14:45:52 +0000

    Fade gifs randomly

--- EverybodyWantsToBeACat/script.js
+++ EverybodyWantsToBeACat/script.js
@@ -1,11 +1,14 @@
 const brick = document.querySelectorAll('.brick');
+console.log(brick.length)
+const random = [0, 5, 3, 10, 4, 9, 2, 6, 1, 8, 11, 4, 14, 7, 12, 13, 15 ]
 
 let timeout = 0;
-for(let i = 0; i < brick.length; i++){
+for(let i = 0; i <= brick.length; i++){
     setTimeout(function(){
-        console.log("hello")
-        brick[i].style.height = "0";
+        console.log(i)
+        brick[random[i]].style.opacity = "0";
     }, timeout);
 
-    timeout += 2000;
+    timeout += 500;
 }
+
