1986d9debe26b1b39a91b40df90a23251e6742ca
LeoRoma <xiajtn@gmail.com>
2021-03-15 15:11:36 +0000

    Added wallpaper as image

--- EverybodyWantsToBeACat/script.js
+++ EverybodyWantsToBeACat/script.js
@@ -1,6 +1,6 @@
 const brick = document.querySelectorAll('.brick');
 console.log(brick.length)
-const random = [0, 5, 3, 10, 4, 9, 2, 6, 1, 8, 11, 4, 14, 7, 12, 13, 15 ]
+const random = [0, 5, 3, 10, 4, 12, 2, 6, 13, 8, 11, 4, 14, 7, 9, 1, 15, 3 ]
 
 let timeout = 0;
 for(let i = 0; i <= brick.length; i++){
