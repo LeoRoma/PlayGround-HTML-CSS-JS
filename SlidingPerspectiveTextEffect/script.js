414550a6dfbf82a68484e537f9a30dbb6a1de47c
LeoRoma <xiajtn@gmail.com>
2021-05-10 16:57:45 +0100

    Change name on hover completed

--- SlidingPerspectiveTextEffect/script.js
+++ SlidingPerspectiveTextEffect/script.js
@@ -3,26 +3,17 @@ console.log(textOut[0]);
 const textOnHover = document.querySelector('.perspective-line');
 
 textOnHover.addEventListener('mouseover', () => {
-    textOut[0].style.transform = "translateY(-100%)";
-    textOut[0].style.transform = "rotateX(0deg)";
-  
+    textOut[0].style.transform = "rotateX(0deg) translateY(50%)";
     textOut[0].style.opacity = 1;
 
-    textOut[1].style.transform = "translateY(0%)";
-    textOut[1].style.transform = "rotateX(-90deg)";
-    // textOut[1].style.transformOrigin = "bottom";
-    
+    textOut[1].style.transform = "rotateX(-90deg) translateY(0%)";
     textOut[1].style.opacity = 0;
 })
 
 textOnHover.addEventListener('mouseout', () => {
-    textOut[0].style.transform = "translateY(-200%)";
-    textOut[0].style.transform = "rotateX(90deg)";
- 
+    textOut[0].style.transform = "rotateX(90deg) translateY(-100%)";
     textOut[0].style.opacity = 0;
  
-    textOut[1].style.transform = "rotateX(0deg)";
-   
-    textOut[1].style.transform = "translateY(-100%)";
+    textOut[1].style.transform = "rotateX(0deg) translateY(-50%)";
     textOut[1].style.opacity = 1;
 })
