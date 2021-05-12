90b4b07e6adec49615c6402544b23b63e0dc5091
LeoRoma <xiajtn@gmail.com>
2021-05-12 11:42:15 +0100

    The image changes on different project title and follows the cursor

--- DisplayImageOnHover/script.js
+++ DisplayImageOnHover/script.js
@@ -1,28 +1,13 @@
-// const projects = document.querySelectorAll('.project');
-// console.log(projects);
-// const projectImages = document.querySelectorAll('.project-image');
-// console.log(projectImages)
+const projects = document.querySelectorAll('.project');
+console.log(projects);
+const projectImages = document.querySelectorAll('.project-image');
+console.log(projectImages)
 
-// const title = document.querySelector('.project-title');
-// for(let i = 0; i < projects.length; i++){
-//     const project = projects[i];
 
-//     project.addEventListener('mousemove', (event) => {
-//         const image = projectImages[i].getAttribute('data-image');
-//         console.log(image)
-//         const imageTag = `<div class="image"><img src="${image}" alt="image" height="100px"</div>`;
-//         project.innerHTML = imageTag
-//         let left = event.pageX + "px";
-//         let top = event.pageY + "px";
-//         img.style.left = left;
-//         img.style.top = top;
-//     })
-// }
+const img = document.querySelectorAll('.image-test');
+console.log(img[0])
 
-const img = document.querySelector('.image-test');
-document.addEventListener('mousemove', getMouseCoords);
-
-setInterval(followMouse, 50);
+// setInterval(followMouse, 50);
 let imageCoords = {
     x: 0,
     y: 0
@@ -38,13 +23,37 @@ function getMouseCoords(event){
     mouseCoords.y = event.pageY;
 }
 
-function followMouse(){
-    let distX = mouseCoords.x - imageCoords.x;
-    let distY = mouseCoords.y - imageCoords.y;
+function followMouse(index){
+    // let distX = mouseCoords.x - imageCoords.x;
+    // let distY = mouseCoords.y - imageCoords.y;
 
-    imageCoords.x += distX / 5;
-    imageCoords.y += distY /2; 
-    
-    img.style.left = imageCoords.x + "px";
-    img.style.top = imageCoords.y + "px";
+    // imageCoords.x += distX / 5;
+    // imageCoords.y += distY /2; 
+    console.log("x: ", mouseCoords.x);
+    console.log("y: ", mouseCoords.y);
+    img[index].style.left = mouseCoords.x + "px";
+    img[index].style.top = mouseCoords.y + "px";
 }
+
+for(let i = 0; i < projects.length; i++){
+    const project = projects[i];
+    project.addEventListener('mouseenter', () => {
+        img[i].style.display = "block";
+        img[i].style.opacity = "1";
+    })
+
+    project.addEventListener('mousemove', (event) => {
+        getMouseCoords(event);
+        followMouse(i);
+     
+    })
+
+    project.addEventListener('mouseleave',() => {
+        img[i].style.display = "hidden";
+        img[i].style.opacity = "0";
+    })
+}
+
+
+// document.addEventListener('mousemove', getMouseCoords);
+
