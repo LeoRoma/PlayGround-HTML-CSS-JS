f296b1198f7eac4eb2d9733d5cf12dc52241c831
LeoRoma <xiajtn@gmail.com>
2021-05-12 10:52:12 +0100

    Can display image from data attribute and can move element with mouse cursor

--- DisplayImageOnHover/script.js
+++ DisplayImageOnHover/script.js
@@ -3,17 +3,21 @@ console.log(projects);
 const projectImages = document.querySelectorAll('.project-image');
 console.log(projectImages)
 
+const title = document.querySelector('.project-title');
 for(let i = 0; i < projects.length; i++){
     const project = projects[i];
 
-    project.addEventListener('mouseenter', () => {
+    project.addEventListener('mousemove', (event) => {
         // const image = projectImages[i].dataset.image;
-        const image = projectImages[i].getAttribute('data-image');
-        console.log(image)
-        const imageTag = `<div class="project-image"><img src="${image}" alt="image" height="100px"</div>`;
-        project.innerHTML = imageTag;
-        // const imgElement = document.querySelector('.set-image');
-        // imgElement.setAttribute("src", image)
-       
+        // const image = projectImages[i].getAttribute('data-image');
+        // console.log(image)
+        // const imageTag = `<div class="project-image"><img src="${image}" alt="image" height="100px"</div>`;
+        // const projectImage = document.createElement("img");
+        // projectImage.src = image;
+        // project.appendChild(projectImage);
+        let left = event.pageX + "px";
+        let top = event.pageY + "px";
+        title.style.left = left;
+        title.style.top = top;
     })
 }
