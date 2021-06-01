c7e3e1c2852f8f9839e496e47aea6a56a59e1e12
LeoRoma <xiajtn@gmail.com>
2021-06-01 19:24:23 +0100

    Barba CDN added

---
+++ PageTransitionBarba/script.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+    const transitionEl = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a');
+    const text = document.querySelector('.hide-text h1');
+
+    setTimeout(() => {
+        transitionEl.classList.remove('is-active');
+    }, 500);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+  
+            let target = e.target.href;
+
+            transitionEl.classList.add('is-active');
+            text.style.top = "100%"
+            
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500)
+        })
+    }
+}
+
+
