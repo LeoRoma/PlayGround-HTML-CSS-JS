67ce4d98b99e60944d1bbf6769fbd7f33ca977d4
LeoRoma <xiajtn@gmail.com>
2021-06-10 07:23:40 +0100

    Deleted comments

--- SlidingPerspectiveTextEffect/scriptBase.js
+++ SlidingPerspectiveTextEffect/scriptBase.js
@@ -7,40 +7,6 @@ const text1 = textOut[1];
 const textOnHover = document.querySelector('.perspective-line');
 
 
-// textOnHover.addEventListener('mouseover', () => {
-
-//     textOut[0].style.transform = "rotateX(0deg) translateY(100%)";
-//     textOut[0].style.opacity = 1;
-
-//     textOut[1].style.transform = " translateY(100%) rotateX(-90deg)";
-//     textOut[1].style.opacity = 0;
-// })
-
-// textOnHover.addEventListener('mouseout', () => {
-
-//     textOut[0].style.transform = "rotateX(90deg) translateY(-100%)";
-//     textOut[0].style.opacity = 0;
- 
-//     textOut[1].style.transform = "rotateX(0deg) translateY(0%)";
-//     textOut[1].style.opacity = 1;
-// })
-
-
-
-
-// textOnHover.addEventListener('mouseover', () => {
-//     animateCharsIn();
-// })
-
-
-// textOnHover.addEventListener('mouseout', () => {
-//     animateCharsOut()
-// })
-
-// let animateCharsTimeline = gsap.timeline({
-//     defaults: {duration: 0.5, ease: "power2", stagger: 0.025}
-// })
-
 function animateCharsIn() {
     gsap.timeline({
         defaults: {duration: 0.5, ease: "power2", stagger: 0.025}
