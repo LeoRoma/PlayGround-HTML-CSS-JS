abdd2e66f9d6f3db5a500542eb27ebf01f62ce95
LeoRoma <xiajtn@gmail.com>
2021-06-21 15:17:26 +0100

    Deleted comments

--- PageTransition/script.js
+++ PageTransition/script.js
@@ -7,12 +7,8 @@ const slider = document.querySelector('.slider');
 
 
 window.onload = function(){
-    // landingpageImage.style.width = "20%";
     slider.classList.add('add-width');
-    // slider.style.width = "20%"
     landingpageImage.classList.add('expand-vertical');
-    // landingpageImage.classList.add('expand-horizontal');
-    // landingpageImage.style.transform = "scaleY(0%, 80%)"
     textSlideIn();
 }
 
@@ -20,9 +16,7 @@ function textSlideIn(){
     let timeout = 0
     for(let i = 0; i < landingpageText.length; i++){
         setTimeout(function(){
-            // landingpageText[i].classList.add('text-slidein');
             landingpageText[i].style.transform = "translateX(-1390px)"
-            // console.log(landingpageText[i])
         }, timeout)
         timeout += 200;
     }
